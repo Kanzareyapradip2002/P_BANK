8075ab033a557ebf7376bca9cbf7acd20919233e
Kanzareyapradip2002 <pradippradip26808@gmail.com>
2024-12-21 13:24:50 +0530

    Update userSignin.js

--- controller/User/userSignin.js
+++ controller/User/userSignin.js
@@ -30,11 +30,7 @@ async function userSignInController(req, res) {
                 email:user.email,
             }
          const token = await jwt.sign(tokenData,process.env.TOKEN_SECRET_KYE, { expiresIn: 60 * 60 * 6 });
-         const tokenOption = {
-            httpOnly:true,
-            secure:true
-         } 
-         res.cookie("token",token,tokenOption).status(200).json({
+         res.cookie("token",token).status(200).json({
             message: "Login Successfully",
             data:token,
             success:true,
