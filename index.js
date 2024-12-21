d0a1c4bae9e223e70e4e98274c4e90c44005bb6d
Kanzareyapradip2002 <pradippradip26808@gmail.com>
2024-12-21 11:52:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(cors({
 }));
 app.use(express.json())
 app.use(cookieParser())
-app.use("/api", router);
+app.use("https://bankbackend-dh51.onrender.com/api", router);
 
 
 connectDB().then(() => {
