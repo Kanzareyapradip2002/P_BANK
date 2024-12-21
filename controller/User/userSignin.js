ea148ac186252338a189e228ba462abcac525e9f
Kanzareyapradip2002 <pradippradip26808@gmail.com>
2024-12-21 17:54:24 +0530

    added

--- controller/User/userSignin.js
+++ controller/User/userSignin.js
@@ -1,12 +1,10 @@
-const bcrypt = require("bcryptjs")
-const userModel = require("../../models/userModel")
-const jwt = require('jsonwebtoken')
-
-
+const bcrypt = require("bcryptjs");
+const userModel = require("../../models/userModel");
+const jwt = require("jsonwebtoken");
 
 async function userSignInController(req, res) {
     try {
-        const { email, password } = req.body
+        const { email, password } = req.body;
 
         if (!email) {
             throw new Error("Please provide Email");
@@ -19,39 +17,48 @@ async function userSignInController(req, res) {
         const user = await userModel.findOne({ email });
 
         if (!user) {
-            throw new Error("User not Found")
+            throw new Error("User not Found");
         }
 
-        const checkPassword = await bcrypt.compare(password, user.password)
+        // Check if the password matches
+        const checkPassword = await bcrypt.compare(password, user.password);
 
         if (checkPassword) {
             const tokenData = {
-                _id:user._id,
-                email:user.email,
-            }
-         const token = await jwt.sign(tokenData,process.env.TOKEN_SECRET_KYE, { expiresIn: 60 * 60 * 6 });
-         const tokenOption = {
-            httpOnly:true,
-            secure:true
-         } 
-         res.cookie("token",token,tokenOption).status(200).json({
-            message: "Login Successfully",
-            data:token,
-            success:true,
-            error:false
-         })
+                _id: user._id,
+                email: user.email,
+            };
+
+            // Sign the JWT token
+            const token = await jwt.sign(tokenData, process.env.TOKEN_SECRET_KEY, { expiresIn: 60 * 60 * 6 });
+
+            // Set cookie options
+            const tokenOptions = {
+                httpOnly: true,      // Prevent access via JavaScript
+                secure: process.env.NODE_ENV === 'production', // Set to true in production, false in development
+                maxAge: 1000 * 60 * 60 * 6,  // Expiry in ms (6 hours)
+                sameSite: 'Strict'   // Optional: helps to prevent CSRF attacks
+            };
+
+            // Send the cookie with the token
+            res.cookie("token", token, tokenOptions).status(200).json({
+                message: "Login Successful",
+                data: token,
+                success: true,
+                error: false,
+            });
 
         } else {
-            throw new Error("Please check Password")
+            throw new Error("Please check your Password");
         }
     } catch (err) {
-        res.json({
-            message: err.message || err,
+        console.error(err);
+        res.status(400).json({
+            message: err.message || "An error occurred",
             error: true,
             success: false,
         });
     }
-
 }
 
-module.exports = userSignInController
+module.exports = userSignInController;
